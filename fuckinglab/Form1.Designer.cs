5bb96cccb0d892eb86527d516360028dfa681e60
Ilin <fatgooses5@gmail.com>
2016-10-23 19:36:23 +0300

    <changes tems on 23.10>

--- fuckinglab/Form1.Designer.cs
+++ fuckinglab/Form1.Designer.cs
@@ -28,12 +28,220 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle8 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle9 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle10 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle11 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle12 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle13 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle14 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridView1 = new MetroFramework.Controls.MetroGrid();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.справкаToolStripButton = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Crimson;
+            this.button1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.button1.Location = new System.Drawing.Point(0, 190);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(522, 47);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Отличники";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(1, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(41, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "Информатика";
+            this.Column6.Name = "Column6";
+            this.Column6.ReadOnly = true;
+            this.Column6.Width = 106;
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "Матем-ка";
+            this.Column5.Name = "Column5";
+            this.Column5.ReadOnly = true;
+            this.Column5.Width = 81;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "Физика";
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            this.Column4.Width = 68;
+            // 
+            // Column3
+            // 
+            this.Column3.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.ColumnHeader;
+            dataGridViewCellStyle8.Font = new System.Drawing.Font("Papyrus", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column3.DefaultCellStyle = dataGridViewCellStyle8;
+            this.Column3.HeaderText = "Курс";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 53;
+            // 
+            // Column2
+            // 
+            this.Column2.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle9.Font = new System.Drawing.Font("Lucida Calligraphy", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column2.DefaultCellStyle = dataGridViewCellStyle9;
+            this.Column2.HeaderText = "ФИО";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 54;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.AllCells;
+            dataGridViewCellStyle10.Font = new System.Drawing.Font("Bookman Old Style", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle10;
+            this.Column1.HeaderText = "Группа";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 67;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToOrderColumns = true;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            dataGridViewCellStyle11.Font = new System.Drawing.Font("Papyrus", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle11;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.AllCells;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dataGridView1.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.None;
+            this.dataGridView1.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle12.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle12.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(17)))), ((int)(((byte)(65)))));
+            dataGridViewCellStyle12.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle12.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(19)))), ((int)(((byte)(73)))));
+            dataGridViewCellStyle12.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle12.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle12;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            dataGridViewCellStyle13.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle13.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle13.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle13.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(136)))), ((int)(((byte)(136)))), ((int)(((byte)(136)))));
+            dataGridViewCellStyle13.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(19)))), ((int)(((byte)(73)))));
+            dataGridViewCellStyle13.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle13.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle13;
+            this.dataGridView1.EnableHeadersVisualStyles = false;
+            this.dataGridView1.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.dataGridView1.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.dataGridView1.Location = new System.Drawing.Point(1, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle14.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle14.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(209)))), ((int)(((byte)(17)))), ((int)(((byte)(65)))));
+            dataGridViewCellStyle14.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            dataGridViewCellStyle14.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle14.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(234)))), ((int)(((byte)(19)))), ((int)(((byte)(73)))));
+            dataGridViewCellStyle14.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(17)))), ((int)(((byte)(17)))), ((int)(((byte)(17)))));
+            dataGridViewCellStyle14.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.RowHeadersDefaultCellStyle = dataGridViewCellStyle14;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView1.Size = new System.Drawing.Size(521, 189);
+            this.dataGridView1.Style = MetroFramework.MetroColorStyle.Red;
+            this.dataGridView1.TabIndex = 3;
+            this.dataGridView1.Theme = MetroFramework.MetroThemeStyle.Light;
+            this.dataGridView1.UseStyleColors = true;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.справкаToolStripButton});
+            this.toolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.VerticalStackWithOverflow;
+            this.toolStrip1.Location = new System.Drawing.Point(9, 65);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(24, 25);
+            this.toolStrip1.TabIndex = 7;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // справкаToolStripButton
+            // 
+            this.справкаToolStripButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image;
+            this.справкаToolStripButton.Image = ((System.Drawing.Image)(resources.GetObject("справкаToolStripButton.Image")));
+            this.справкаToolStripButton.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.справкаToolStripButton.Name = "справкаToolStripButton";
+            this.справкаToolStripButton.Size = new System.Drawing.Size(22, 20);
+            this.справкаToolStripButton.Text = "Спр&авка";
+            this.справкаToolStripButton.Click += new System.EventHandler(this.справкаToolStripButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(522, 237);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Cursor = System.Windows.Forms.Cursors.PanNW;
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.HelpButton = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Text = "IlinLab15-16";
+            this.HelpButtonClicked += new System.ComponentModel.CancelEventHandler(this.Form1_HelpButtonClicked);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private MetroFramework.Controls.MetroGrid dataGridView1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton справкаToolStripButton;
     }
 }
 
--- fuckinglab/Form1.cs
+++ fuckinglab/Form1.cs
@@ -7,14 +7,182 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using System.Diagnostics;
 namespace fuckinglab
 {
+
     public partial class Form1 : Form
     {
         public Form1()
         {
             InitializeComponent();
         }
+        #region
+        public class Student
+        {
+            //свойства - есть строки таблицы
+            public string group { get; set; }
+            public string name { get; set; }
+            public int value1 { get; set; }
+            public int subject1 { get; set; }
+            public int subject2 { get; set; }
+            public int subject3 { get; set; }
+
+            public Student(string gr, string nm, int v, int s1, int s2, int s3)
+            {
+                group = gr;
+                name = nm;
+                value1 = v;
+                subject1 = s1;
+                subject2 = s2;
+                subject3 = s3;
+            }
+        }
+        public List<Student> liststudent = new List<Student>();
+
+        #endregion
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+           
+            #region
+            if (textBox1.Text.ToString() != "")
+            {
+                int i = Int32.Parse(textBox1.Text.ToString());
+                dataGridView1.Rows.Clear();
+
+                foreach (Student s in liststudent)
+                {
+                    if ((s.subject1 == i) && (s.subject2 == i) && (s.subject3 == i))
+                    {
+                        dataGridView1.Rows.Add(s.group.ToString(), s.name.ToString(), s.value1.ToString(), s.subject1.ToString(), s.subject2.ToString(), s.subject3.ToString());
+                    }
+                }
+                #endregion
+                #region
+                // for (int i = 0; i < dataGridView1.RowCount; i ++)
+                //{ richTextBox1.Text = ", " + Convert.ToString(dataGridView1.Rows[i].Cells[0].Value) + "\r\n" + richTextBox1.Text;
+                //}
+                /*  foreach (DataGridViewCell item in this.dataGridView1.CurrentRow.Cells)
+                  {
+                      if (item.ColumnIndex == 4)
+                          this.richTextBox1.Text += item.Value.ToString();
+                      if (item.ColumnIndex == 5)
+                          this.richTextBox1.Text += item.Value.ToString();
+                      if (item.ColumnIndex == 6)
+                          this.richTextBox1.Text += item.Value.ToString();
+                  }*/
+                // if ((dataGridView1.Rows[1].Cells[3] = 5 && (dataGridView1.Rows[1].Cells[4] = 5) && (dataGridView1.Rows[1].Cells[5] = 5)))
+
+                //    }
+                #endregion
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            #region
+
+
+            DataGridViewRow row1 = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+            row1.Cells[0].Value = "11 ПКС";
+            row1.Cells[1].Value = "Ильин М.С.";
+            row1.Cells[2].Value = "1";
+            row1.Cells[3].Value = "5";
+            row1.Cells[4].Value = "5";
+            row1.Cells[5].Value = "5";
+            dataGridView1.Rows.Add(row1);
+
+            DataGridViewRow row2 = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+            row2.Cells[0].Value = "11ПКС";
+            row2.Cells[1].Value = "Крамской И.К.";
+            row2.Cells[2].Value = "1";
+            row2.Cells[3].Value = "5";
+            row2.Cells[4].Value = "5";
+            row2.Cells[5].Value = "5";
+            dataGridView1.Rows.Add(row2);
+
+            DataGridViewRow row3 = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+            row3.Cells[0].Value = "11 ПКС";
+            row3.Cells[1].Value = "Кожушная В.В.";
+            row3.Cells[2].Value = "1";
+            row3.Cells[3].Value = "4";
+            row3.Cells[4].Value = "4";
+            row3.Cells[5].Value = "4";
+            dataGridView1.Rows.Add(row3);
+
+            DataGridViewRow row4 = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+            row4.Cells[0].Value = "11 ПКС";
+            row4.Cells[1].Value = "Королев М.Г.";
+            row4.Cells[2].Value = "1";
+            row4.Cells[3].Value = "5";
+            row4.Cells[4].Value = "4";
+            row4.Cells[5].Value = "3";
+            dataGridView1.Rows.Add(row4);
+
+            DataGridViewRow row5 = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+            row5.Cells[0].Value = "11 ПКС";
+            row5.Cells[1].Value = "Меньшиков Х.З.";
+            row5.Cells[2].Value = "1";
+            row5.Cells[3].Value = "3";
+            row5.Cells[4].Value = "3";
+            row5.Cells[5].Value = "3";
+
+            dataGridView1.Rows.Add(row5);
+
+            DataGridViewRow row6 = (DataGridViewRow)dataGridView1.Rows[0].Clone();
+            row6.Cells[0].Value = "11 ПКС";
+            row6.Cells[1].Value = "Прахов В.В.";
+            row6.Cells[2].Value = "1";
+            row6.Cells[3].Value = "5";
+            row6.Cells[4].Value = "5";
+            row6.Cells[5].Value = "5";
+
+            dataGridView1.Rows.Add(row6);
+#endregion
+
+            /*   #region
+          for (int i = 0; i <= dataGridView1.Rows.Count; i++)
+            {
+                liststudent.Add(new Student(dataGridView1.Rows[i].Cells[0].Value.ToString(), dataGridView1.Rows[i].Cells[1].Value.ToString(), Int32.Parse(dataGridView1.Rows[i].Cells[2].Value.ToString()), Int32.Parse(dataGridView1.Rows[i].Cells[3].Value.ToString()), Int32.Parse(dataGridView1.Rows[i].Cells[4].Value.ToString()), Int32.Parse(dataGridView1.Rows[i].Cells[5].Value.ToString())));
+
+                #endregion
+            }*/
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+           
+        }
+
+        private void Form1_HelpButtonClicked(object sender, CancelEventArgs e)
+        {
+            #region
+            Process webpage = new Process();
+            AboutBox1 aboutpro = new AboutBox1();
+            aboutpro.Show();
+            #endregion
+            #region
+
+
+
+
+         //   Process.Start(@"E:\трпо\15-16\fuckinglab\fuckinglab\HTMLPage1.htm");
+
+
+
+            #endregion
+            Help.ShowHelp(this, "filehelp.chm");
+
+
+        }
+
+        private void справкаToolStripButton_Click(object sender, EventArgs e)
+        {
+            Help.ShowHelp(this,"filehelp.chm");
+        }
     }
 }
+   
